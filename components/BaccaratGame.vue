da478c73274814ef66ede2bd1535c80e7482f9de
DESKTOP-361N8F5\USer <virak.sukadigital@gmail.com>
2022-03-15 15:19:22 +0700

    - create page baccarat-game
    - create CardComponent

---
+++ components/BaccaratGame.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+         <div id="baccarat-game" class="margin-space">
+            <cardComponents :baccaratGames="gameLink" :isActive="isActive"  />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        gameLink: Array,
+    },
+    data() {
+        return {
+             components: {
+                 cardComponents:() => import('@/components/CardComponents'),
+             },
+             isActive: true,
+        }
+    },
+    
+}
+</script>
---
+++ components/CardComponents.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="display-card" v-for="(baccaratGame, index) in baccaratGames" :key="index">
+            <b-card
+                :img-src="baccaratGame.image"
+                img-alt=""
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+                <span v-if="isActive" class="btn-theme gd-red-black no-border border-raduis-18" href="#" >Go somewhere</span>
+                <span v-else href="#" >Go somewhere</span>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        baccaratGames: Array,
+        isActive: Boolean,
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
