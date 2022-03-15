9c11379ac7c1aae9ccb9a6a39cf2d9e91c4a8824
DESKTOP-361N8F5\USer <virak.sukadigital@gmail.com>
2022-03-15 15:28:37 +0700

    - create page Casino

---
+++ pages/casino.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="home-default-wrapper">
+
+        <Header />
+        <RunningText />
+        <div class="container bg-gray-lighter">
+            <OffCanvasMobileMenu />
+            <CasinoContainer />
+<!--            <HeroOne />-->
+
+<!--            <ServiceOne />-->
+
+<!--            <CausesOne />-->
+
+<!--            <FunFactOne />-->
+
+<!--            <Sponsor />-->
+
+            <client-only>
+                <back-to-top class="scroll-top" bottom="30px">
+                    <i class="arrow-top icofont-rounded-up"></i>
+                    <i class="arrow-bottom icofont-rounded-up"></i>
+                </back-to-top>
+            </client-only>
+        </div>
+        
+        <!-- <Footer /> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            Header: () => import('@/components/Header'),
+            OffCanvasMobileMenu: () => import('@/components/OffCanvasMobileMenu'),
+            Breadcrumb: () => import('@/components/Breadcrumb'),
+            PricingWrapper: () => import('@/components/PricingWrapper'),
+            CasinoContainer: () => import('@/components/Casino/CasinoContainer'),
+            // Footer: () => import('@/components/Footer'),
+        },
+
+        data() {
+            return {
+                items: [
+                    {
+                        text: 'Home',
+                        to: "/"
+                    },
+                    {
+                        text: 'casino',
+                        active: true
+                    }
+                ]
+            }
+        },
+
+        head() {
+            return {
+                title: 'Pricing'
+            }
+        },
+    };
+</script>
+
+
